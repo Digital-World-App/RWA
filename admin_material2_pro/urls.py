9c9ced90b5d1a372e5737bd1c978376b0e0c6f10
ASPPIBRA-DEV <sandro_ceo@asppibra.com.br>
2024-09-27 20:57:53 -0300

    Gênesis

---
+++ admin_material2_pro/urls.py
@@ -0,0 +1,67 @@
+from django.urls import path
+from admin_material2_pro import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    # Dashboard
+    path('', views.analytics, name="index"),    
+    path('sales/', views.sales, name="sales"),    
+    path('smart-home/', views.smart_home, name="smart_home"),
+
+    
+    # Applications
+    path('applications/crm/', views.crm, name="crm"),   
+    path('applications/kanban/', views.kanban, name="kanban"),   
+    path('applications/wizard/', views.wizard, name="wizard"),   
+    path('applications/datatables/', views.datatables, name="datatables"),   
+    path('applications/calendar/', views.calendar, name="calendar"),   
+    path('applications/stats/', views.stats, name="stats"), 
+
+    
+    # Authentication -> Login
+    path('accounts/basic-login/', views.BasicLoginView.as_view(), name="basic_login"),
+    path('accounts/cover-login/', views.CoverLoginView.as_view(), name="cover_login"),
+    path('accounts/illustration-login/', views.IllustrationLoginView.as_view(), name="illustration_login"),
+
+    # Authentication -> Register
+    path('accounts/basic-register/', views.basic_register, name="basic_register"),
+    path('accounts/cover-register/', views.cover_register, name="cover_register"),
+    path('accounts/illustration-register/', views.illustration_register, name="illustration_register"),
+
+    # Authentication -> Reset
+    path('accounts/basic-reset/', views.BasicPasswordResetView.as_view(), name="basic_reset"),
+    path('accounts/cover-reset/', views.CoverPasswordResetView.as_view(), name="cover_reset"),
+    path('accounts/illustration-reset/', views.IllustrationPasswordResetView.as_view(), name="illustration_reset"),
+
+    path('accounts/password-change/', views.UserPasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password-change-done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='accounts/done/change-done.html'
+    ), name="password_change_done"),
+    path('accounts/password-reset-done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/done/basic.html'
+    ), name='password_reset_done'),
+    path('accounts/password-reset-confirm/<uidb64>/<token>/', 
+        views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('accounts/password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/complete/basic.html'
+    ), name='password_reset_complete'),
+
+    # Authentication -> Verification
+    path('accounts/basic-verification/', views.basic_verification, name="basic_verification"),
+    path('accounts/cover-verification/', views.cover_verification, name="cover_verification"),
+    path('accounts/illustration-verification/', views.illustration_verification, name="illustration_verification"),
+
+    # Authentication -> Lock
+    path('accounts/basic-lock/', views.basic_lock, name="basic_lock"),
+    path('accounts/cover-lock/', views.cover_lock, name="cover_lock"),
+    path('accounts/illustration-lock/', views.illustration_lock, name="illustration_lock"),
+
+    # Error
+    path('error/404/', views.error_404, name="error_404"),
+    path('error/500/', views.error_500, name="error_500"),
+
+    # Logout
+    path('accounts/logout/', views.logout_view, name='logout'),
+]
+
---
+++ admin_material2_pro/views.py
@@ -0,0 +1,186 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordChangeView, PasswordResetConfirmView
+from admin_material2_pro.forms import RegistrationForm, LoginForm, UserPasswordResetForm, UserSetPasswordForm, UserPasswordChangeForm
+from django.contrib.auth import logout
+
+# Create your views here.
+
+# Dashboard
+def analytics(request):
+  context = {
+    'parent': 'dashboard',
+    'segment': 'analytics'
+  }
+  return render(request, 'pages/dashboards/analytics.html', context)
+
+def sales(request):
+  context = {
+    'parent': 'dashboard',
+    'segment': 'sales'
+  }
+  return render(request, 'pages/dashboards/sales.html', context)
+
+def smart_home(request):
+  context = {
+    'parent': 'dashboard',
+    'segment': 'smart_home'
+  }
+  return render(request, 'pages/dashboards/smart-home.html', context)
+
+
+
+
+# Applications
+def crm(request):
+  context = {
+    'parent': 'applications',
+    'segment': 'crm'
+  }
+  return render(request, 'pages/applications/crm.html', context)
+  
+def kanban(request):
+  context = {
+    'parent': 'applications',
+    'segment': 'kanban'
+  }
+  return render(request, 'pages/applications/kanban.html', context)
+
+def wizard(request):
+  context = {
+    'parent': 'applications',
+    'segment': 'wizard'
+  }
+  return render(request, 'pages/applications/wizard.html', context)
+
+def datatables(request):
+  context = {
+    'parent': 'applications',
+    'segment': 'datatables'
+  }
+  return render(request, 'pages/applications/datatables.html', context)
+
+def calendar(request):
+  context = {
+    'parent': 'applications',
+    'segment': 'calendar'
+  }
+  return render(request, 'pages/applications/calendar.html', context)
+
+def stats(request):
+  context = {
+    'parent': 'applications',
+    'segment': 'stats'
+  }
+  return render(request, 'pages/applications/stats.html', context)
+
+
+# Authentication -> Login
+class BasicLoginView(LoginView):
+  template_name = 'accounts/signin/basic.html'
+  form_class = LoginForm
+
+class CoverLoginView(LoginView):
+  template_name = 'accounts/signin/cover.html'
+  form_class = LoginForm
+
+class IllustrationLoginView(LoginView):
+  template_name = 'accounts/signin/illustration.html'
+  form_class = LoginForm
+
+# Authentication -> Register
+def basic_register(request):
+  if request.method == 'POST':
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      print('Account created successfully!')
+      return redirect('/accounts/basic-login/')
+    else:
+      print("Register failed!")
+  else:
+    form = RegistrationForm()
+
+  context = { 'form': form }
+  return render(request, 'accounts/signup/basic.html', context)
+
+def cover_register(request):
+  if request.method == 'POST':
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      print('Account created successfully!')
+      return redirect('/accounts/cover-login/')
+    else:
+      print("Register failed!")
+  else:
+    form = RegistrationForm()
+
+  context = { 'form': form }
+  return render(request, 'accounts/signup/cover.html', context)
+
+def illustration_register(request):
+  if request.method == 'POST':
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      print('Account created successfully!')
+      return redirect('/accounts/illustration-login/')
+    else:
+      print("Register failed!")
+  else:
+    form = RegistrationForm()
+
+  context = { 'form': form }
+  return render(request, 'accounts/signup/illustration.html', context)
+
+# Authentication -> Lock
+def basic_lock(request):
+  return render(request, 'accounts/lock/basic.html')
+
+def cover_lock(request):
+  return render(request, 'accounts/lock/cover.html')
+
+def illustration_lock(request):
+  return render(request, 'accounts/lock/illustration.html')
+
+# Authentication -> Reset
+class BasicPasswordResetView(PasswordResetView):
+  template_name = 'accounts/reset/basic.html'
+  form_class = UserPasswordResetForm
+
+class CoverPasswordResetView(PasswordResetView):
+  template_name = 'accounts/reset/cover.html'
+  form_class = UserPasswordResetForm
+
+class IllustrationPasswordResetView(PasswordResetView):
+  template_name = 'accounts/reset/illustration.html'
+  form_class = UserPasswordResetForm
+
+class UserPasswordResetConfirmView(PasswordResetConfirmView):
+  template_name = 'accounts/reset-confirm/basic.html'
+  form_class = UserSetPasswordForm
+
+class UserPasswordChangeView(PasswordChangeView):
+  template_name = 'accounts/change/basic.html'
+  form_class = UserPasswordChangeForm
+
+# Authentication -> Verification
+def basic_verification(request):
+  return render(request, 'accounts/verification/basic.html')
+
+def cover_verification(request):
+  return render(request, 'accounts/verification/cover.html')
+
+def illustration_verification(request):
+  return render(request, 'accounts/verification/illustration.html')
+
+# Error
+def error_404(request):
+  return render(request, 'accounts/error/404.html')
+
+def error_500(request):
+  return render(request, 'accounts/error/500.html')
+
+def logout_view(request):
+  logout(request)
+  return redirect('/accounts/basic-login/')
